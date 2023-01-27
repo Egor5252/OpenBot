1a1b84ea44e8fea7909f87c488e2ae3f4787ba12
Egor5252 <123667969+Egor5252@users.noreply.github.com>
2023-01-27 22:27:56 +0300

    1

--- main.py
+++ main.py
@@ -27,58 +27,8 @@ def handle_message(message):
         bot.send_message(chat_id=message.from_user.id,
                          text=GoogleTranslator(source='auto', target='ru').translate(response['choices'][0]['text']))
     except:
-        try:
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=message.text,
-                temperature=0.5,
-                max_tokens=4000,
-                top_p=1,
-                frequency_penalty=0.5,
-                presence_penalty=0.0,
-            )
-            print(response['choices'][0]['text'])
-            # print(GoogleTranslator(source='auto', target='ru').translate(response['choices'][0]['text']))
-            bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
-            bot.send_message(chat_id=message.from_user.id, text=GoogleTranslator(source='auto', target='ru').translate(
-                response['choices'][0]['text']))
-        except:
-            try:
-                response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt=message.text,
-                    temperature=0.5,
-                    max_tokens=4000,
-                    top_p=1,
-                    frequency_penalty=0.5,
-                    presence_penalty=0.0,
-                )
-                print(response['choices'][0]['text'])
-                # print(GoogleTranslator(source='auto', target='ru').translate(response['choices'][0]['text']))
-                bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
-                bot.send_message(chat_id=message.from_user.id,
-                                 text=GoogleTranslator(source='auto', target='ru').translate(
-                                     response['choices'][0]['text']))
-            except:
-                try:
-                    response = openai.Completion.create(
-                        model="text-davinci-003",
-                        prompt=message.text,
-                        temperature=0.5,
-                        max_tokens=4000,
-                        top_p=1,
-                        frequency_penalty=0.5,
-                        presence_penalty=0.0,
-                    )
-                    print(response['choices'][0]['text'])
-                    # print(GoogleTranslator(source='auto', target='ru').translate(response['choices'][0]['text']))
-                    bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
-                    bot.send_message(chat_id=message.from_user.id,
-                                     text=GoogleTranslator(source='auto', target='ru').translate(
-                                         response['choices'][0]['text']))
-                except ZeroDivisionError:
-                    print('Ошибка')
-                    bot.send_message(chat_id=message.from_user.id, text='Повторите попытку')
+        print('Ошибка')
+        bot.send_message(chat_id=message.from_user.id, text='Повторите попытку')
 
 
 print("Бот запущен")

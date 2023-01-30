5bf32100d7a88206073f01f67fe01944857a7481
Egor5252 <123667969+Egor5252@users.noreply.github.com>
2023-01-30 04:19:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ import mes
 
 openai.api_key = APIs.API_KEY_OPENAI  # Api ключ OpenAI
 bot = telebot.TeleBot(APIs.API_KEY_TELEGRAM_BOT)  # Api ключ Telegram бота
+file = "f'Logs/{message.from_user.id}.txt'"
 
 
 @bot.message_handler(commands=['start', 'clear'])
 def handle_message(message):
     print('Команда обнаружена')
     if message.text == '/clear':
-        f = open(f'Logs/{message.from_user.id}.txt', 'w+')
+        f = open(file, 'a')
         f.write(mes.mes)
         f.close()
         bot.send_message(chat_id=message.from_user.id, text='Память бота очищена')
     elif message.text == '/start':
-        f = open(f'Logs/{message.from_user.id}.txt', 'w+')
+        f = open(f'Logs/{message.from_user.id}.txt', 'w')
         f.write(mes.mes)
         f.close()
         bot.send_message(chat_id=message.from_user.id, text='Задавайте любые вопрысы!')
@@ -25,8 +26,9 @@ def handle_message(message):
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    f = open(f'Logs/{message.from_user.id}.txt', 'w+')
-    zapros = f.read() + f'\nYou: {message.text}\nComputer:'
+    f = open(f'Logs/{message.from_user.id}.txt', 'r')
+    zapros = f.read() + '\nYou: ' + message.text + '\nComputer:'
+    f.close()
     try:
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -43,6 +45,7 @@ def handle_message(message):
                          text=GoogleTranslator(source='auto', target='ru').translate(response['choices'][0]['text']))
         zapros = zapros + response['choices'][0]['text']
         f.write(zapros)
+        f.close()
     except:
         bot.send_message(chat_id=message.from_user.id, text='Ошибка')
 

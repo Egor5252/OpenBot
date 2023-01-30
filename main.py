017980ae1db7f2651659de0dbc11947be74ca7b7
Egor5252 <123667969+Egor5252@users.noreply.github.com>
2023-01-30 04:09:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ bot = telebot.TeleBot(APIs.API_KEY_TELEGRAM_BOT)  # Api ключ Telegram бот
 def handle_message(message):
     print('Команда обнаружена')
     if message.text == '/clear':
-        mes.mes = 'You: Are elephants really afraid of mice?\nComputer: No, elephants are not afraid of mice. Elephants are ' \
-                  'actually quite tolerant of small animals and have been known to interact with them in a friendly manner.' \
-                  '\nYou: Who don`t they like?\nComputer: ' \
-                  'Elephants are generally not fond of large predators such as lions, tigers, and leopards. They can also become ' \
-                  'agitated around humans if they feel threatened or scared.'
+        f = open(f'Logs/{message.from_user.id}.txt', 'w+')
+        f.write(mes.mes)
+        f.close()
         bot.send_message(chat_id=message.from_user.id, text='Память бота очищена')
     elif message.text == '/start':
         f = open(f'Logs/{message.from_user.id}.txt', 'w+')
         f.write(mes.mes)
+        f.close()
         bot.send_message(chat_id=message.from_user.id, text='Задавайте любые вопрысы!')
 
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    mes.mes = mes.mes + '\nYou: ' + message.text + '\nComputer:'
+    f = open(f'Logs/{message.from_user.id}.txt', 'w+')
+    zapros = f.read() + f'\nYou: {message.text}\nComputer:'
     try:
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -41,10 +41,9 @@ def handle_message(message):
         bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
         bot.send_message(chat_id=message.from_user.id,
                          text=GoogleTranslator(source='auto', target='ru').translate(response['choices'][0]['text']))
-        mes.mes = mes.mes + response['choices'][0]['text']
-        print(mes.mes)
+        zapros = zapros + response['choices'][0]['text']
+        f.write(zapros)
     except:
-        print(mes.mes)
         bot.send_message(chat_id=message.from_user.id, text='Ошибка')
 
 

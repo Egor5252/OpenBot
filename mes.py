4e82bcc2af3c2325f804eca8f3b4dfd5d291a9ff
Egor5252 <123667969+Egor5252@users.noreply.github.com>
2023-02-27 12:52:52 +0300

    Update mes.py

--- mes.py
+++ mes.py
@@ -1,8 +1,4 @@
 mes = 'You: Are elephants really afraid of mice?' \
-      '\nComputer: No, elephants are not afraid of mice. Elephants are ' \
-      'actually quite tolerant of small animals and have been known to ' \
-      'interact with them in a friendly manner.' \
+      '\nComputer: No, elephants are not afraid of mice. Elephants are actually quite tolerant of small animals and have been known to interact with them in a friendly manner.' \
       '\nYou: Who don`t they like?' \
-      '\nComputer: Elephants are generally not fond of large predators ' \
-      'such as lions, tigers, and leopards. They can also become ' \
-      'agitated around humans if they feel threatened or scared.'
+      '\nComputer: Elephants are generally not fond of large predators such as lions, tigers, and leopards. They can also become agitated around humans if they feel threatened or scared.'
